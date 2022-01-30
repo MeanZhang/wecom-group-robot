35061f051e3f03150e150c9f33706f69d9e26758
Mean <390602272@qq.com>
2022-01-30 16:50:00 +0800

    Squashed commit of the following:
    commit e48f41f51df687926fc68b833943d973fd7907fb
    Author: Mean <390602272@qq.com>
    Date:   Sun Jan 30 16:36:43 2022 +0800
    
        📃 docs: 修改README
    
    commit c12382a451964f992920fb7d9bc38aff258b9501
    Author: Mean <390602272@qq.com>
    Date:   Sun Jan 30 15:33:35 2022 +0800
    
        🐎 ci: 修改codecov配置
    
    commit f960100c6464cc76de0d0fa582bbaf099b74e5ba
    Author: Mean <390602272@qq.com>
    Date:   Sun Jan 30 11:18:47 2022 +0800
    
        🐎 ci: 增加codecov报告上传

--- index.js
+++ index.js
@@ -5,65 +5,72 @@ const path = require("path");
 const FormData = require("form-data");
 
 async function run() {
-  try {
-    const key = core.getInput("key");
-    const url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + key;
-    const msgtype = core.getInput("msgtype");
-    const content = core.getInput("content");
-    const params = {
-      msgtype: msgtype,
-    };
-    switch (msgtype) {
-      // TODO mentioned
-      case "text":
-      case "markdown":
-        params[msgtype] = { content: content };
-        break;
-      case "image":
-        break;
-      case "news":
-        break;
-      case "file": {
-        const upload_url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=${key}&type=file`;
-        const file = fs.createReadStream(content);
-        const filename = path.basename(content);
-        let filelength = 0;
-        fs.stat(content, (err, stats) => {
-          filelength = stats.size;
-        });
-        const form = new FormData();
-        form.append("file", file);
-        const res = await axios({
-          method: "post",
-          url: upload_url,
-          data: form,
-          headers: {
-            "Content-Disposition": `form-data; name="media";filename=${filename}; filelength=${filelength}`,
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        if (res.data.errcode === 0) {
-          params[msgtype] = { media_id: res.data.media_id };
-        } else {
-          throw new Error(res.data);
-        }
-        break;
-      }
-      default:
-        throw new Error("invalid msgtype: " + msgtype);
-    }
-    axios.post(url, params).then((response) => {
-      if (response.data.errcode === 0) {
-        core.setOutput("errcode", 0);
-        core.setOutput("errmsg",response.data.errmsg)
+  const key = core.getInput("key");
+  const url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + key;
+  const msgtype = core.getInput("msgtype");
+  const content = core.getInput("content");
+  const params = {
+    msgtype: msgtype,
+  };
+  switch (msgtype) {
+    // TODO mentioned
+    case "text":
+    case "markdown":
+      params[msgtype] = { content: content };
+      break;
+    // case "image":
+    //   break;
+    // case "news":
+    //   break;
+    case "file": {
+      const upload_url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=${key}&type=file`;
+      const file = fs.createReadStream(content);
+      const filename = path.basename(content);
+      let filelength = 0;
+      fs.stat(content, (err, stats) => {
+        filelength = stats.size;
+      });
+      const form = new FormData();
+      form.append("file", file);
+      const res = await axios({
+        method: "post",
+        url: upload_url,
+        data: form,
+        headers: {
+          "Content-Disposition": `form-data; name="media";filename=${filename}; filelength=${filelength}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (res.data.errcode === 0) {
+        params[msgtype] = { media_id: res.data.media_id };
       } else {
-        core.debug(response.data);
-        core.setFailed(response.data);
+        core.setFailed(res.data);
+        return res.data;
       }
-    });
-  } catch (error) {
-    core.setFailed(error.message);
+      break;
+    }
+    default:
+      core.setFailed({
+        errcode: -2,
+        errmsg: "invalid msgtype: " + msgtype,
+      });
+      return {
+        errcode: -2,
+        errmsg: "invalid msgtype: " + msgtype,
+      };
   }
+  const response = await axios.post(url, params);
+  if (response.data.errcode === 0) {
+    core.setOutput("errcode", 0);
+    core.setOutput("errmsg", response.data.errmsg);
+  } else {
+    core.setFailed(response.data);
+  }
+  return response.data;
 }
 
 run();
+
+module.exports = {
+  run,
+};
--- index.test.js
+++ index.test.js
@@ -1,25 +1,40 @@
 const process = require("process");
-const cp = require("child_process");
-const path = require("path");
 const fs = require("fs");
+const { run } = require("./index");
 
 process.env["INPUT_KEY"] = process.env["TEST_KEY"];
 
 test("测试文本", () => {
   process.env["INPUT_MSGTYPE"] = "text";
   process.env["INPUT_CONTENT"] = "文本测试\n" + new Date().toTimeString();
-  const ip = path.join(__dirname, "index.js");
-  const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
-  console.log(result);
+  return run().then((out) => {
+    expect(out.errcode).toBe(0);
+  });
 });
 
 test("测试 Markdown", () => {
   process.env["INPUT_MSGTYPE"] = "markdown";
   process.env["INPUT_CONTENT"] =
     "### Markdown 测试\n" + new Date().toTimeString();
-  const ip = path.join(__dirname, "index.js");
-  const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
-  console.log(result);
+  return run().then((out) => {
+    expect(out.errcode).toBe(0);
+  });
+});
+
+test("测试图片", () => {
+  process.env["INPUT_MSGTYPE"] = "image";
+  process.env["INPUT_CONTENT"] = "test.png";
+  return run().then((out) => {
+    expect(out.errcode).toBe(-2);
+  });
+});
+
+test("测试新闻", () => {
+  process.env["INPUT_MSGTYPE"] = "news";
+  process.env["INPUT_CONTENT"] = "test";
+  return run().then((out) => {
+    expect(out.errcode).toBe(-2);
+  });
 });
 
 test("测试文件", () => {
@@ -27,10 +42,30 @@ test("测试文件", () => {
   const filename = "testfile-" + getTimeString() + ".md";
   fs.writeFileSync(filename, "### Markdown 测试\n" + new Date().toTimeString());
   process.env["INPUT_CONTENT"] = filename;
-  const ip = path.join(__dirname, "index.js");
-  const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
-  console.log(result);
-  fs.unlinkSync(filename)
+  return run().then((out) => {
+    fs.unlinkSync(filename);
+    expect(out.errcode).toBe(0);
+  });
+});
+
+test("测试空文件", () => {
+  process.env["INPUT_MSGTYPE"] = "file";
+  const filename = "testfile-" + getTimeString() + ".md";
+  fs.writeFileSync(filename, "");
+  process.env["INPUT_CONTENT"] = filename;
+  return run().then((out) => {
+    fs.unlinkSync(filename);
+    expect(out.errcode).not.toBe(0);
+  });
+});
+
+test("测试错误 key", () => {
+  process.env["INPUT_KEY"] = "111";
+  process.env["INPUT_MSGTYPE"] = "text";
+  process.env["INPUT_CONTENT"] = "test";
+  return run().then((out) => {
+    expect(out.errcode).not.toBe(0);
+  });
 });
 
 function getTimeString() {

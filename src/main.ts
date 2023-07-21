6f2179089e23f61bd67cce406d108edd426c753a
Mean <390602272@qq.com>
2023-07-21 16:54:11 +0800

    feat: 文件类型支持@actions/glob匹配

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,74 @@
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
 import FormData from 'form-data'
 import axios from 'axios'
 import crypto from 'crypto'
 import fs from 'fs'
 import path from 'path'
 
+type MsgType = 'text' | 'markdown' | 'image' | 'file'
+
+function isMsgType(msgtype: string): msgtype is MsgType {
+  return ['text', 'markdown', 'image', 'file'].includes(msgtype)
+}
+
+async function pushFiles(
+  paths: string
+): Promise<{errcode: number; errmsg: string}> {
+  const globber = await glob.create(paths)
+  const files = await globber.glob()
+  const key = core.getInput('key')
+  for (const file_path of files) {
+    const upload_url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=${key}&type=file`
+    const file = fs.createReadStream(file_path)
+    const filename = path.basename(file_path)
+    const stats = fs.statSync(file_path)
+    if (stats.isDirectory()) {
+      continue
+    }
+    const filelength = stats.size
+    const form = new FormData()
+    form.append('file', file)
+    const res = await axios({
+      method: 'post',
+      url: upload_url,
+      data: form,
+      headers: {
+        'Content-Disposition': `form-data; name="media"; filename=${filename}; filelength=${filelength}`,
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    if (res.data.errcode === 0) {
+      const params = {
+        msgtype: 'file',
+        file: {
+          media_id: res.data.media_id
+        }
+      }
+      const url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${key}`
+      await axios.post(url, params)
+    } else {
+      core.setFailed(res.data)
+      return res.data
+    }
+  }
+  return {
+    errcode: 0,
+    errmsg: 'ok'
+  }
+}
+
 export async function run(): Promise<{errcode: number; errmsg: string}> {
   const key = core.getInput('key')
   const url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${key}`
   const msgtype = core.getInput('msgtype')
+  if (!isMsgType(msgtype)) {
+    core.setFailed(`invalid msgtype: ${msgtype}`)
+    return {
+      errcode: -1,
+      errmsg: `invalid msgtype: ${msgtype}`
+    }
+  }
   const content = core.getInput('content')
   const params: {
     msgtype: string
@@ -42,39 +102,8 @@ export async function run(): Promise<{errcode: number; errmsg: string}> {
     }
     // case "news":
     //   break;
-    case 'file': {
-      const upload_url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key=${key}&type=file`
-      const file = fs.createReadStream(content)
-      const filename = path.basename(content)
-      let filelength = 0
-      fs.stat(content, (err, stats) => {
-        filelength = stats.size
-      })
-      const form = new FormData()
-      form.append('file', file)
-      const res = await axios({
-        method: 'post',
-        url: upload_url,
-        data: form,
-        headers: {
-          'Content-Disposition': `form-data; name="media";filename=${filename}; filelength=${filelength}`,
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      if (res.data.errcode === 0) {
-        params[msgtype] = {media_id: res.data.media_id}
-      } else {
-        core.setFailed(res.data)
-        return res.data
-      }
-      break
-    }
-    default:
-      core.setFailed(`invalid msgtype: ${msgtype}`)
-      return {
-        errcode: -1,
-        errmsg: `invalid msgtype: ${msgtype}`
-      }
+    case 'file':
+      return pushFiles(content)
   }
   const response = await axios.post(url, params)
   if (response.data.errcode !== 0) {
